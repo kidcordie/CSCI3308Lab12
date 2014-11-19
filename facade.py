b1da2e1dbe6b909d8ad096c151f570aa097b227c
user <user@CU-CS-VM.(none)>
2014-11-19 15:51:43 -0700

    finished lab 12

--- facade.py
+++ facade.py
@@ -5,19 +5,24 @@ import parts
 # You should implement this class
 # This class should be able to be called by the running part
 class Computer:
-    def __init__(self):
-        self.cpu=parts.CPU()
-        self.mem=parts.Memory()
-        self.hd=parts.HardDisk()
+	def __init__(self):
+		self.cpu=parts.CPU()
+		self.mem=parts.Memory()
+		self.hd=parts.HardDisk()
     # implement the class below this line    
-
-
-
-
-
-
-
-
+	def startComputer(self):
+		self.cpu.check()
+		self.mem.load()
+		self.hd.mount()
+		
+	def printCPUInfo(self):
+		self.cpu.getInfo()
+		
+	def printMemInfo(self):
+		self.mem.getInfo()
+		
+	def printHDInfo(self):
+		self.hd.getInfo()
 # The running part
 # Don't modify the following code!
 def main():
